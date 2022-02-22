dc86192a3bbdf7eba97155b1447d26f095e25592
mrFlagman <Sergej_neo@gmx.de>
2022-02-22 21:58:09 +0100

    Update horoscopes DOM structure
    Update the structure of HTML elements on the horoscopes website, so this parsing modul works again.

--- horoscope/pyhoroscope.py
+++ horoscope/pyhoroscope.py
@@ -1,104 +1,110 @@
-import urllib.request
-from lxml import etree
-import re
+from datetime import datetime, timezone
+
+import requests
+from lxml import html
 
 ####################################################################
 # API
 ####################################################################
 
-class Horoscope:
+def get_todays_horoscope(sunsign):
+    url = "http://www.ganeshaspeaks.com/horoscopes/daily-horoscope/" + sunsign
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+    # date = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+    date = str(tree.xpath("//*[@id=\"main\"]/section/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/p/text()"))
+    date = date.replace("']", "").replace("['", "")
+    date_utc = datetime.now(timezone.utc)
+    date_website = "-".join(date.split('-')[::-1])
+    date_local = str(date_utc.astimezone()).split(' ')[0]
 
-    @staticmethod
-    def get_todays_horoscope(sunsign):
-        url = "http://www.ganeshaspeaks.com/horoscopes/daily-horoscope/" + sunsign
-        response = urllib.request.urlopen(url)
-        htmlparser = etree.HTMLParser()
-        tree = etree.parse(response, htmlparser)
-        date = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        date = date.replace("['(", "").replace(")']", "")
+    if date_local < date_website:
+        url = "https://www.ganeshaspeaks.com/horoscopes/yesterday-horoscope/" + sunsign
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
+    elif date_local > date_website:
+        url = "https://www.ganeshaspeaks.com/horoscopes/tomorrow-horoscope/" + sunsign
+        response = requests.get(url)
+        tree = html.fromstring(response.content)
+        # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
+    else:
+        # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+        horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
 
-        date_website = "-".join(date.split('-')[::-1])
-        date_local = str(datetime.now().astimezone()).split(' ')[0]
-        if date_local < date_website :
-            url = "https://www.ganeshaspeaks.com/horoscopes/yesterday-horoscope/" + sunsign
-            response = requests.get(url)
-            tree = html.fromstring(response.content)
-            horoscope = str(tree.xpath(
-                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        elif date_local > date_website :
-            url = "https://www.ganeshaspeaks.com/horoscopes/tomorrow-horoscope/" + sunsign
-            response = requests.get(url)
-            tree = html.fromstring(response.content)
-            horoscope = str(tree.xpath(
-                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        else :
-            horoscope = str(tree.xpath(
-                "//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        horoscope = horoscope.replace("[u'", "").replace("']", "")
-        dict = {
-            'date': date,
-            'horoscope': horoscope,
-            'sunsign': sunsign
-            }
-        return dict
+    horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("[\"", "").replace("\"]", "").replace("[\'",
+                                                                                                             "").replace(
+        "\']", "")
+    dict = {
+        'date': date_local,
+        'horoscope': horoscope,
+        'sunsign': sunsign
+    }
 
-    @staticmethod
-    def get_weekly_horoscope(sunsign):
-        url = "http://www.ganeshaspeaks.com/horoscopes/weekly-horoscope/" + sunsign
-        response = urllib.request.urlopen(url)
-        htmlparser = etree.HTMLParser()
-        tree = etree.parse(response, htmlparser)
-        week = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        week = week.replace("[u'\\n", "").replace("']", "").replace("\\u2013", "-")
-        horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        horoscope = horoscope.replace("['", "").replace("']", "")
-        dict = {
-            'week': week,
-            'horoscope': horoscope,
-            'sunsign': sunsign
-        }
+    return dict
 
-        return dict
 
-    @staticmethod
-    def get_monthly_horoscope(sunsign):
-        url = "http://www.ganeshaspeaks.com/horoscopes/monthly-horoscope/" + sunsign
-        response = urllib.request.urlopen(url)
-        htmlparser = etree.HTMLParser()
-        tree = etree.parse(response, htmlparser)
-        month = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        month = month.replace("['\\n", "").replace("']", "")
-        horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()[1]"))
-        horoscope = horoscope.replace("['", "").replace("']", "")
-        dict = {
-            'month': month,
-            'horoscope': horoscope,
-            'sunsign': sunsign
-        }
+def get_weekly_horoscope(sunsign):
+    url = "http://www.ganeshaspeaks.com/horoscopes/weekly-horoscope/" + sunsign
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+    # week = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+    week = str(tree.xpath("//*[@id=\"main\"]/section/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/p/text()"))
+    week = week.replace("']", "").replace("['", "")
+    # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+    horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
+    horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
+    dict = {
+        'week': week,
+        'horoscope': horoscope,
+        'sunsign': sunsign
+    }
 
-        return dict
+    return dict
 
-    @staticmethod
-    def get_yearly_horoscope(sunsign):
-        url = "http://www.ganeshaspeaks.com/horoscopes/yearly-horoscope/" + sunsign
-        response = urllib.request.urlopen(url)
-        htmlparser = etree.HTMLParser()
-        tree = etree.parse(response, htmlparser)
-        year = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
-        year = year.replace("['\\n", "").replace("']", "")
-        horoscope = str(tree.xpath(
-			"//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
-        horoscope = horoscope.replace("[u'", "").replace("']", "").replace("\\xe2\\x80\\x99s", "")
-        dict = {
-            'year': year,
-            'horoscope': horoscope,
-            'sunsign': sunsign
-        }
 
-        return dict
+def get_monthly_horoscope(sunsign):
+    url = "http://www.ganeshaspeaks.com/horoscopes/monthly-horoscope/" + sunsign
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+    # month = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+    month = str(tree.xpath("//*[@id=\"main\"]/section/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/p/text()"))
+    month = month.replace("']", "").replace("['", "")
+    # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()[1]"))
+    horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
+    horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
+    dict = {
+        'month': month,
+        'horoscope': horoscope,
+        'sunsign': sunsign
+    }
+
+    return dict
+
+
+def get_yearly_horoscope(sunsign):
+    url = "http://www.ganeshaspeaks.com/horoscopes/yearly-horoscope/" + sunsign
+    response = requests.get(url)
+    tree = html.fromstring(response.content)
+    # year = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[1]/div[2]/div/p/text()"))
+    year = str(tree.xpath("//*[@id=\"main\"]/section/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/p/text()"))
+    year = year.replace("']", "").replace("['", "")
+    # horoscope = str(tree.xpath("//*[@id=\"daily\"]/div/div[1]/div[2]/p[1]/text()"))
+    horoscope = str(tree.xpath("//*[@id=\"horo_content\"]/text()"))
+    horoscope = horoscope.replace("\\n", "").replace("  ", "").replace("']", "").replace("['", "")
+    dict = {
+        'year': year,
+        'horoscope': horoscope,
+        'sunsign': sunsign
+    }
+
+    return dict
+
+
+if __name__ == '__main__':
+    print(get_todays_horoscope("pisces"))
+    print(get_weekly_horoscope("pisces"))
+    print(get_monthly_horoscope("pisces"))
+    print(get_yearly_horoscope("pisces"))
